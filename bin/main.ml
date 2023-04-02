94440aa85795a77d919757861d06d06e0be9e26a
Oliver Salman <oliversalman1@gmail.com>
2023-04-02 19:33:23 +1000

    better error messages

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 open Core
 open Poppy
 
+exception ParseError of string [@@ocaml.warning "-38"]
+
 (* Read the input file *)
 let read_file filename =
   In_channel.read_all filename
@@ -37,12 +39,8 @@ let main () =
     print_endline (Sexp.to_string_hum (Ast.sexp_of_statement ast))
   with
   | Sys_error msg -> print_endline ("Error: " ^ msg)
-  | Failure msg -> print_endline ("Syntax error: " ^ msg)
-  | Poppy.Parser.Error ->
-      let curr = (Lexing.from_string "").lex_curr_p in
-      let line = curr.pos_lnum in
-      let cnum = curr.pos_cnum - curr.pos_bol + 1 in
-      Printf.printf "Syntax error at line %d, column %d\n" line cnum
+  | Failure msg -> print_endline ("Syntax Error: " ^ msg)
+  | ParseError msg -> print_endline (msg)
 
 (* Execute the main function *)
 let () = main ()
