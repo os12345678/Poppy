099968a57b6cdfce0b05693f79cad78c9a714345
Oliver Salman <oliversalman1@gmail.com>
2023-04-16 22:43:50 +1000

    add tests

--- test/unit_tests/test_parser.ml
+++ test/unit_tests/test_parser.ml
@@ -27,3 +27,33 @@ let%expect_test "parse_expr" =
   [%expect {|
     (FuncDecl (Id main) () (Type Void)
      ((Expr (BinOp Plus (IntLiteral 1) (IntLiteral 2))))) |}]
+
+let%expect_test "parse_function_call" =
+  let input = "fn main() -> void { foo(); }" in
+  let output = parse_input input in
+  print_endline (to_string output);
+  [%expect {| (FuncDecl (Id main) () (Type Void) ((Expr (Call foo ())))) |}]
+
+let%expect_test "parse_function_call_with_args" =
+  let input = "fn main() -> void { foo(1, 2); }" in
+  let output = parse_input input in
+  print_endline (to_string output);
+  [%expect {|
+    (FuncDecl (Id main) () (Type Void)
+     ((Expr (Call foo ((IntLiteral 1) (IntLiteral 2)))))) |}]
+
+let%expect_test "function_application" = 
+  let input = "
+    fn f(x: int) -> int {
+      x;
+    }
+    fn main() -> void {
+      f(1);
+    }
+  " in
+  let output = parse_input input in
+  print_endline (to_string output);
+  [%expect {|
+    (FuncDecl (Id f) ((Param (Id x) (Type Int))) (Type Int) ((Expr (Id x))))
+    (FuncDecl (Id main) () (Type Void) ((Expr (Call f ((IntLiteral 1)))))) |}]
+
