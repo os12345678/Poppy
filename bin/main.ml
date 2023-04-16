c7951e3a8b3d2edcaaa608221acc4e3cbc5c2ed7
Oliver Salman <oliversalman1@gmail.com>
2023-04-16 15:18:35 +1000

    use exposed parser functions

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,7 @@ open Core
 open Poppy_parser
 open Poppy_codegen
 open Llvm
+open Parser_interface
 
 
 exception ParseError of string [@@ocaml.warning "-38"]
@@ -9,21 +10,6 @@ exception ParseError of string [@@ocaml.warning "-38"]
 (* Read the input file *)
 let read_file filename =
   In_channel.read_all filename
-
-(* Parse the input *)
-let parse_input input filename =
-  let lexbuf = Lexing.from_string input in
-  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-  try
-    Parser.main Lexer.read_tok lexbuf
-  with
-  | Parser.Error ->
-      let curr = lexbuf.lex_curr_p in
-      let line = curr.pos_lnum in
-      let cnum = curr.pos_cnum - curr.pos_bol + 1 in
-      Printf.printf "Syntax error at line %d, column %d\n" line cnum;
-      raise Parser.Error
-
 
 let main () =
   try
@@ -38,7 +24,7 @@ let main () =
     let input = read_file filename in
 
     (* Parse the input *)
-    let ast = parse_input input filename in
+    let ast = parse_input input in
 
     (* Print the parsed AST *)
     print_endline (Sexp.to_string_hum (Ast.sexp_of_statements ast));
