a2717b13af63a05428ff5fafe29200f927d6bd88
Oliver Salman <oliversalman1@gmail.com>
2023-04-16 15:19:06 +1000

    simple test for empty void functions

--- test/unit_tests/test_parser.ml
+++ test/unit_tests/test_parser.ml
@@ -1,8 +1,39 @@
 open Alcotest
 open Poppy_parser.Ast
+open Poppy_parser.Parser_interface
+open Sexplib
 
-let test_parse_int_literal () =
-  let input = "123" in
-  let expected_output = IntLiteral 123 in
-  let actual_output = Parser.parse input in
-  check (option expr) "parses int literal correctly" (Some expected_output) actual_output
+module Testable = struct
+  let cmp_expr e1 e2 = match (e1, e2) with
+    | IntLiteral i1, IntLiteral i2 -> i1 = i2
+    | _ -> false
+
+  let equal_statement s1 s2 = match (s1, s2) with
+    | Expr e1, Expr e2 -> cmp_expr e1 e2
+    | _ -> false
+
+  let statement = Alcotest.testable
+      (fun ppf s ->
+         Fmt.pf ppf "%s" (Sexp.to_string_hum (sexp_of_statement s)))
+      equal_statement
+end
+
+
+let test_parse_empty_void_fn () =
+  let input = "
+  fn main() -> void {
+  }
+  " 
+  in
+  let expected_output = [FuncDecl (Id "main", [], Type Void, [])] in
+  let actual_output = parse_input input in
+  check (list Testable.statement) "parses empty void fn correctly" expected_output actual_output
+
+
+  let suite =
+    [
+      "test_parse_int_literal", `Quick, test_parse_empty_void_fn;
+      (* Add more test cases here *)
+    ]
+  
+  let () = Alcotest.run "Poppy parser tests" [("parser", suite)]
