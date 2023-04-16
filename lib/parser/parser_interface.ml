e43ec911d93dd23ba325c3d3db0c28ce589d292f
Oliver Salman <oliversalman1@gmail.com>
2023-04-16 15:15:54 +1000

    expose parse input function for other parts of the program

---
+++ lib/parser/parser_interface.ml
@@ -0,0 +1,13 @@
+exception ParseError of string
+
+let parse_input input =
+  let lexbuf = Lexing.from_string input in
+  try
+    Parser.main Lexer.read_tok lexbuf
+  with
+  | Lexer.SyntaxError msg ->
+      let pos = lexbuf.lex_curr_p in
+      raise (ParseError (Printf.sprintf "%s at line %d, column %d" msg pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)))
+  | Parser.Error ->
+      let pos = lexbuf.lex_curr_p in
+      raise (ParseError (Printf.sprintf "Unexpected token '%s' at line %d, column %d" (Lexing.lexeme lexbuf) pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)))
