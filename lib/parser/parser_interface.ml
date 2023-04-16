d452dcea7144676ea867029ccd72f9ea069dd830
Oliver Salman <oliversalman1@gmail.com>
2023-04-16 16:06:22 +1000

    added helper function for expect tests

--- lib/parser/parser_interface.ml
+++ lib/parser/parser_interface.ml
@@ -1,4 +1,6 @@
 exception ParseError of string
+(* open Sexplib *)
+(* open Poppy_parser *)
 
 let parse_input input =
   let lexbuf = Lexing.from_string input in
@@ -11,3 +13,9 @@ let parse_input input =
   | Parser.Error ->
       let pos = lexbuf.lex_curr_p in
       raise (ParseError (Printf.sprintf "Unexpected token '%s' at line %d, column %d" (Lexing.lexeme lexbuf) pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)))
+
+open Sexplib
+let to_string (stmts : Ast.statement list) : string =
+  stmts |> List.map Ast.sexp_of_statement
+        |> List.map Sexp.to_string_hum
+        |> String.concat "\n"
