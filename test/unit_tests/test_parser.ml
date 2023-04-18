74f8f16a97c6d92c44afc8715b1f84d1bc394456
Oliver Salman <oliversalman1@gmail.com>
2023-04-18 20:55:53 +1000

    update tests

--- test/unit_tests/test_parser.ml
+++ test/unit_tests/test_parser.ml
@@ -4,45 +4,35 @@ let%expect_test "parse_empty_fn" =
   let input = "fn main() -> void {}" in
   let output = parse_input input in
   print_endline (to_string output);
-  [%expect {| (FuncDecl (Id main) () (Type Void) ()) |}]
-
-let%expect_test "parse_empty_fn_with_args" =
-  let input = "fn main(a: int, b: int) -> void {}" in
-  let output = parse_input input in
-  print_endline (to_string output);
-  [%expect {|
-    (FuncDecl (Id main) ((Param (Id a) (Type Int)) (Param (Id b) (Type Int)))
-     (Type Void) ()) |}]
+  [%expect {| (MainFunc ()) |}]
 
 let%expect_test "parse_int" = 
   let input = "fn main() -> void { 123; }" in
   let output = parse_input input in
   print_endline (to_string output);
-  [%expect {| (FuncDecl (Id main) () (Type Void) ((Expr (IntLiteral 123)))) |}]
+  [%expect {| (MainFunc ((Expr (IntLiteral 123)))) |}]
 
 let%expect_test "parse_expr" =
   let input = "fn main() -> void { 1 + 2; }" in
   let output = parse_input input in
   print_endline (to_string output);
   [%expect {|
-    (FuncDecl (Id main) () (Type Void)
-     ((Expr (BinOp Plus (IntLiteral 1) (IntLiteral 2))))) |}]
+    (MainFunc ((Expr (BinOp Plus (IntLiteral 1) (IntLiteral 2))))) |}]
 
 let%expect_test "parse_function_call" =
   let input = "fn main() -> void { foo(); }" in
   let output = parse_input input in
   print_endline (to_string output);
-  [%expect {| (FuncDecl (Id main) () (Type Void) ((Expr (Call foo ())))) |}]
+  [%expect {| (MainFunc ((Expr (Call foo ())))) |}]
 
 let%expect_test "parse_function_call_with_args" =
   let input = "fn main() -> void { foo(1, 2); }" in
   let output = parse_input input in
   print_endline (to_string output);
   [%expect {|
-    (FuncDecl (Id main) () (Type Void)
-     ((Expr (Call foo ((IntLiteral 1) (IntLiteral 2)))))) |}]
+    (MainFunc ((Expr (Call foo ((IntLiteral 1) (IntLiteral 2)))))) |}]
 
-let%expect_test "function_application" = 
+let%expect_test "function_expression_application" = 
   let input = "
     fn f(x: int) -> int {
       x;
@@ -55,5 +45,25 @@ let%expect_test "function_application" =
   print_endline (to_string output);
   [%expect {|
     (FuncDecl (Id f) ((Param (Id x) (Type Int))) (Type Int) ((Expr (Id x))))
-    (FuncDecl (Id main) () (Type Void) ((Expr (Call f ((IntLiteral 1)))))) |}]
+    (MainFunc ((Expr (Call f ((IntLiteral 1)))))) |}]
 
+let%expect_test "function_statement_application" = 
+  let input = "
+    fn f(x: int) -> bool {
+      if(x < 5){
+        return True;
+      } else {
+        return False;
+      }
+    }
+    fn main() -> void {
+      f(1);
+    }
+  " in
+  let output = parse_input input in
+  print_endline (to_string output);
+  [%expect {|
+    (FuncDecl (Id f) ((Param (Id x) (Type Int))) (Type Bool)
+     ((If (BinOp Lt (Id x) (IntLiteral 5)) (Block ((Return (BoolLiteral true))))
+       (Block ((Return (BoolLiteral false)))))))
+    (MainFunc ((Expr (Call f ((IntLiteral 1)))))) |}]
