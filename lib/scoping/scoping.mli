3df9b73a5e5866fa5694ce6aa899d6019477f33d
Oliver Salman <oliversalman1@gmail.com>
2023-04-28 22:48:48 +1000

    update interface

--- lib/scoping/scoping.mli
+++ lib/scoping/scoping.mli
@@ -3,11 +3,12 @@ exception DuplicateMemberVariable of string
 exception DuplicateMemberMethod of string
 exception DuplicateLocalVariable of string
 type value =
-    Variable of Poppy_parser.Ast.expr
-  | Parameter of Poppy_parser.Ast.expr
-  | ReturnValue of Poppy_parser.Ast.expr
+    Variable of Poppy_parser.Ast.expr * Poppy_parser.Ast.typ
+  | Parameter of Poppy_parser.Ast.expr * Poppy_parser.Ast.typ
+  | ReturnValue of Poppy_parser.Ast.expr * Poppy_parser.Ast.typ
   | ClassInstance of class_info * (string, value) Hashtbl.t
 and class_info = {
+  class_name : string;
   parent_class : class_info option;
   class_scope : scope;
   member_variables : (string, value) Hashtbl.t;
@@ -21,9 +22,17 @@ and scope = {
 }
 val create_instance : class_info -> value
 val create_new_scope : scope option -> scope
-val add_class : scope -> string -> class_info -> unit
+val create_new_class_info : string -> class_info option -> class_info
+val add_class : scope -> string -> class_info option -> unit
+val add_identifier : scope -> string -> Poppy_parser.Ast.typ -> unit
 val add_member_variable : class_info -> string -> value -> unit
 val add_member_method :
   class_info -> string -> Poppy_parser.Ast.statement -> string list -> unit
 val add_local_variable : scope -> string -> value -> unit
-val find_identifier : scope -> string -> value option
+val find_identifier : scope -> string -> Poppy_parser.Ast.typ option
+val find_expr_type : Poppy_parser.Ast.expr -> Poppy_parser.Ast.typ
+val find_return_type : Poppy_parser.Ast.statement -> Poppy_parser.Ast.typ
+val find_method_return_type :
+  class_info -> string -> Poppy_parser.Ast.typ option
+val find_class : scope -> string -> class_info option
+val find_member_variable : class_info -> string -> value option
