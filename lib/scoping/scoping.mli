bd1505a1d42b8e6dce7694825cb60f594dae058c
Oliver Salman <oliversalman1@gmail.com>
2023-05-05 18:38:45 +1000

    update scoping interface

--- lib/scoping/scoping.mli
+++ lib/scoping/scoping.mli
@@ -11,9 +11,13 @@ and class_info = {
   class_name : string;
   parent_class : class_info option;
   class_scope : scope;
-  member_variables : (string, value) Hashtbl.t;
+  member_variables :
+    (string, Poppy_parser.Ast.access_modifier * value) Hashtbl.t;
   member_methods :
-    (string, Poppy_parser.Ast.statement * string list) Hashtbl.t;
+    (string,
+     Poppy_parser.Ast.access_modifier *
+     (Poppy_parser.Ast.statement * string list))
+    Hashtbl.t;
 }
 and scope = {
   parent : scope option;
@@ -22,12 +26,17 @@ and scope = {
 }
 val create_instance : class_info -> value
 val create_new_scope : scope option -> scope
+val scope_level : scope -> int
 val create_new_class_info : string -> class_info option -> class_info
-val add_class : scope -> string -> class_info option -> unit
+val add_class : scope -> string -> class_info -> unit
 val add_identifier : scope -> string -> Poppy_parser.Ast.typ -> unit
-val add_member_variable : class_info -> string -> value -> unit
+val add_member_variable :
+  class_info -> string -> Poppy_parser.Ast.access_modifier * value -> unit
 val add_member_method :
-  class_info -> string -> Poppy_parser.Ast.statement -> string list -> unit
+  class_info ->
+  string ->
+  Poppy_parser.Ast.access_modifier *
+  (Poppy_parser.Ast.statement * string list) -> unit
 val add_local_variable : scope -> string -> value -> unit
 val find_identifier : scope -> string -> Poppy_parser.Ast.typ option
 val find_expr_type : Poppy_parser.Ast.expr -> Poppy_parser.Ast.typ
@@ -35,4 +44,15 @@ val find_return_type : Poppy_parser.Ast.statement -> Poppy_parser.Ast.typ
 val find_method_return_type :
   class_info -> string -> Poppy_parser.Ast.typ option
 val find_class : scope -> string -> class_info option
-val find_member_variable : class_info -> string -> value option
+val get_global_scope : scope -> scope
+val find_member_variable :
+  class_info -> string -> (Poppy_parser.Ast.access_modifier * value) option
+val find_member_variable_in_class_and_ancestors :
+  class_info -> string -> (Poppy_parser.Ast.access_modifier * value) option
+val string_of_typ : Poppy_parser.Ast.typ -> string
+val string_of_value : value -> string
+val string_of_expr : Poppy_parser.Ast.expr -> string
+val string_of_binop : Poppy_parser.Ast.bin_op -> string
+val string_of_access_modifier : Poppy_parser.Ast.access_modifier -> string
+val string_of_statement : Poppy_parser.Ast.statement -> string
+val print_scope_contents : scope -> int -> unit
