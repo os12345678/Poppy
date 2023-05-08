b38972f27c36d52d686b65290f1dbca432ef186a
Oliver Salman <oliversalman1@gmail.com>
2023-05-08 21:24:13 +1000

    started codegen expressions

--- lib/ir/codegen.ml
+++ lib/ir/codegen.ml
@@ -1,45 +1,55 @@
-(* open Llvm
+open Llvm
 open Poppy_parser.Ast
 open Scoping
-open Sexplib
+(* open Sexplib *)
 open Core
+(* open Poppy_type_checker *)
+open Poppy_codegen__Codegen_util
 
 exception Codegen_error of string
 
 
 (* Module and context creation *)
-let context = Poppy_codegen__Codegen_util.context
-let builder = Poppy_codegen__Codegen_util.builder
-let the_module = create_module context "Poppy"
+let context = context
+let builder = builder
+let the_module = the_module
 let named_values : (string, llvalue) Stdlib.Hashtbl.t = Stdlib.Hashtbl.create 10
+let function_protos: (string, (string * func_param list * type_decl) option) Stdlib.Hashtbl.t = Stdlib.Hashtbl.create 50
+let class_protos: (string, class_info) Stdlib.Hashtbl.t = Stdlib.Hashtbl.create 50
 
 (* Utility functions for code generation *)
-let rec codegen_expr (current_scope : scope) (expr : expr) (current_class_info : class_info) : Llvm.llvalue =
-match expr with
-| IntLiteral i -> const_int (i32_type context) i
+let rec codegen_expr (expr: expr) (scope: scope) (class_info: class_info) (builder: Llvm.llbuilder) (func_map: (string, Llvm.llvalue) Stdlib.Hashtbl.t): Llvm.llvalue =
+  match expr with
+| IntLiteral i -> const_int (i64_type context) i
 | BoolLiteral b -> const_int (i1_type context) (if b then 1 else 0)
-| StringType s -> build_global_stringptr s "str" builder
-| StringLiteral s -> build_global_stringptr s "str" builder
+| StringLiteral s ->
+  let str = Llvm.build_global_stringptr s "global_string" builder in
+  str
 | VoidType -> const_null (void_type context)
 | Unit -> const_null (void_type context)
 
 | Id id ->
-  (match Poppy_codegen__Codegen_util.find_named_value id named_values with
+  (match find_named_value id named_values with
    | Some value -> value
    | None -> failwith (Printf.sprintf "Unbound variable: %s" id))
 
 | BinOp (op, e1, e2) ->
-  let left = codegen_expr current_scope e1 current_class_info in
-  let right = codegen_expr current_scope e2 current_class_info in
-  Poppy_codegen__Codegen_util.codegen_binop op left right
+  let left = codegen_expr e1 scope class_info builder func_map in
+  let right = codegen_expr e2 scope class_info builder func_map in
+  codegen_binop op left right
 
+| Call (func_name, args) ->
+  let llvm_args = List.map ~f:(fun arg -> codegen_expr arg scope class_info builder func_map) args in
+  codegen_call func_name llvm_args func_map
+  
+(*
 | ClassInstantiation (var_name, class_name, exprs) ->
   (match find_class current_scope class_name with
   | Some class_info ->
     let instance = create_instance class_info in
     codegen_class_instance instance
   | None -> failwith (Printf.sprintf "Class not found: %s" class_name))
-  
+
 | ClassMemberAccess (instance_expr, member_name) ->
   let instance = codegen_expr current_scope instance_expr current_class_info in
   codegen_class_member_access instance member_name current_class_info
@@ -51,12 +61,10 @@ match expr with
   codegen_call current_scope func_name args current_class_info
 
 | This ->
-  codegen_this current_scope current_class_info
-
-| unimplemented_expression ->
-  failwith (Printf.sprintf "Code generation not implemented for this expression: %s" (Sexp.to_string_hum (sexp_of_expr unimplemented_expression)))
+  codegen_this current_scope current_class_info *)
 
   | unimplement_expression ->
     let sexp = sexp_of_expr unimplement_expression in
     let expr_str = Sexp.to_string_hum sexp in
-    raise (Failure ("expression not implemented: " ^ expr_str)) *)
+    raise (Failure ("expression not implemented: " ^ expr_str))
+
--- lib/ir/codegen_util.ml
+++ lib/ir/codegen_util.ml
@@ -1,17 +1,37 @@
 open Llvm
 open Poppy_parser.Ast
 open Scoping
-(* open Poppy_type_checker *)
 
+(* ############################# LLVM setup ################################# *)
 let context = global_context ()
 let builder = builder context
+let the_module = create_module context "Poppy"
 
-(* Helper functions for codegen *)
+(* ############################# LLVM types ################################# *)
+let rec llvm_type_of_typ context = function
+  | Int -> i64_type context
+  | Bool -> i1_type context
+  | Void -> void_type context
+  | String -> pointer_type (i8_type context)
+  | Function (param_types, return_type) ->
+    let llvm_param_types = Array.of_list (List.map (fun typ -> llvm_type_of_typ context typ) param_types) in
+    let llvm_return_type = llvm_type_of_typ context return_type in
+    function_type llvm_return_type llvm_param_types
+  | _ -> raise (Failure (Printf.sprintf "llvm_type_of_typ: ClassInstance not supported yet"))
+
+(* ###################### Helper functions for codegen ###################### *)
+
+let func_map : (string, Llvm.llvalue) Stdlib.Hashtbl.t = Stdlib.Hashtbl.create 10
 let is_pointer (llvm_type: lltype) : bool =
   classify_type llvm_type = TypeKind.Pointer
-;;
 
-(* Auxiliary functions for codegen *)
+  let find_function (function_map: (string, llvalue) Hashtbl.t) (func_name: string) : llvalue option =
+    try
+      Some (Hashtbl.find function_map func_name)
+    with
+      Not_found -> None
+
+(* ####################### Auxiliary Codegen Expr ########################### *)
 let find_named_value (id : string) (named_values : (string, llvalue) Hashtbl.t) : llvalue option =
   try
     Some (Hashtbl.find named_values id)
@@ -42,3 +62,22 @@ let codegen_class_instantiation (current_scope: scope) (_var_name: string) (clas
     | ClassInstance (instance_class_info, _) -> codegen_class_instance instance_class_info
     | _ -> raise (Failure (Printf.sprintf "Unexpected value type when creating instance of class %s" class_name)))
   | None -> raise (Failure (Printf.sprintf "Class %s not found" class_name))
+
+let codegen_call func_name args func_map =
+  let func = match find_function func_map func_name with
+    | Some func -> func
+    | None -> raise (Failure (Printf.sprintf "Function %s not found" func_name))
+  in
+  let params = params func in
+  let args = List.map2 (fun param arg ->
+    let param_type = type_of param in
+    let arg_type = type_of arg in
+    if param_type = arg_type then arg
+    else if is_pointer param_type && arg_type = i64_type context then
+      build_inttoptr arg param_type "inttoptrtmp" builder
+    else if param_type = i64_type context && is_pointer arg_type then
+      build_ptrtoint arg param_type "ptrtointtmp" builder
+    else raise (Failure (Printf.sprintf "Type mismatch in function call %s" func_name))
+  ) (Array.to_list params) args in
+  build_call func (Array.of_list args) "calltmp" builder
+  
