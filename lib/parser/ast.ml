ec38caa9724b117de8df8c666a5cb7058617ce8e
Oliver Salman <oliversalman1@gmail.com>
2023-05-14 13:32:54 +1000

    update @@deriving sexp_of to @@deriving sexp

--- lib/parser/ast.ml
+++ lib/parser/ast.ml
@@ -1,13 +1,13 @@
 open! Core
 open Ast_types
 
-type identifier = Variable of Var_name.t [@@deriving sexp_of]
+type identifier = Variable of Var_name.t [@@deriving sexp]
 
 type expr = {
   loc : loc;
   node: expr_node
 }
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 and expr_node =
 | Int                 of int
@@ -23,17 +23,17 @@ and expr_node =
 | For                 of expr * expr * expr * block_expr
 | BinOp               of bin_op * expr * expr
 | UnOp                of un_op * expr
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 and block_expr = Block of loc * expr list
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 and constructor_arg = ConstructorArg of Field_name.t * expr
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 type function_definition = (*Function of Function_name.t * borrowed_ref option * type_expr * param list * block_expr*)
 | Function of Function_name.t * param list * type_expr * block_expr
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 (*Method of Method_name.t * borrowed_ref option * type_expr * param list * Capability_name.t list * block_expr*)
 type method_defn =
@@ -42,16 +42,16 @@ type method_defn =
     * param list
     * type_expr
     * block_expr
-    [@@deriving sexp_of]
+    [@@deriving sexp]
 
 type class_definition = (*Class of Class_name.t * generic_type option * Class_name.t option * capability list * field_defn list * method_defn list*)
 | Class of
     Class_name.t
     * method_defn list
-    [@@deriving sexp_of]
+    [@@deriving sexp]
 
 type program = Prog of class_definition list * function_definition list * block_expr
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 let sexp_of_expressions expr =
   Sexp.List (List.map expr ~f:sexp_of_expr_node)
--- lib/parser/ast_types.ml
+++ lib/parser/ast_types.ml
@@ -3,54 +3,58 @@ open Core
 type loc = {
   lnum : int;
   cnum : int;
-} [@@deriving sexp_of]
+} [@@deriving sexp]
 
 
 let loc_of_position {Lexing.pos_lnum; pos_cnum; pos_bol; _} =
   { lnum = pos_lnum; cnum = pos_cnum - pos_bol + 1 }
 
+let string_of_loc {lnum; cnum} = sprintf "l:%d, c:%d" lnum cnum
+
 module type ID = sig
-  type t [@@deriving sexp_of]
+  type t [@@deriving sexp]
 
   val of_string : string -> t
   val to_string : t -> string
   val ( = ) : t -> t -> bool
+  val compare: t -> t -> int
 end
 
 module String_id = struct
-  type t = string [@@deriving sexp_of]
+  type t = string [@@deriving sexp]
 
   let of_string x = x
   let to_string x = x
   let ( = ) = String.( = )
+  let compare = String.compare
 end
 
-module Var_name : ID = String_id [@@deriving sexp_of]
-module Class_name : ID = String_id [@@deriving sexp_of]
-module Capability_name : ID = String_id [@@deriving sexp_of]
-module Field_name : ID = String_id [@@deriving sexp_of]
-module Method_name : ID = String_id [@@deriving sexp_of]
-module Function_name : ID = String_id [@@deriving sexp_of]
+module Var_name : ID = String_id [@@deriving sexp]
+module Class_name : ID = String_id [@@deriving sexp]
+module Capability_name : ID = String_id [@@deriving sexp]
+module Field_name : ID = String_id [@@deriving sexp]
+module Method_name : ID = String_id [@@deriving sexp]
+module Function_name : ID = String_id [@@deriving sexp]
 
 type mode =
  | Linear
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
 let string_of_mode = function
  | Linear       -> "Linear"
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
-type modifier = MConst | MVar [@@deriving sexp_of]
+type modifier = MConst | MVar [@@deriving sexp]
 
-let string_of_modifier = function MConst -> "Const" | MVar -> "Var" [@@deriving sexp_of]
+let string_of_modifier = function MConst -> "Const" | MVar -> "Var" [@@deriving sexp]
 
-type borrowed_ref = Borrowed [@@deriving sexp_of]
+type borrowed_ref = Borrowed [@@deriving sexp]
 
-let string_of_maybe_borrowed_ref = function Some Borrowed -> "Borrowed " | None -> "" [@@deriving sexp_of]
+let string_of_maybe_borrowed_ref = function Some Borrowed -> "Borrowed " | None -> "" [@@deriving sexp]
 
-type generic_type = Generic [@@deriving sexp_of]
+type generic_type = Generic [@@deriving sexp]
 
-let string_of_maybe_generic = function Some Generic -> "<T>" | None -> "" [@@deriving sexp_of]
+let string_of_maybe_generic = function Some Generic -> "<T>" | None -> "" [@@deriving sexp]
 
 type type_expr =
   | TEInt
@@ -58,7 +62,7 @@ type type_expr =
   | TEVoid
   | TEBool
   | TEGeneric
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
 let rec string_of_type = function
   | TEInt -> "Int"
@@ -71,22 +75,22 @@ let rec string_of_type = function
   | TEVoid -> "Void"
   | TEBool -> "Bool"
   | TEGeneric -> "T"
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
-type field_defn = TField of modifier * type_expr * Field_name.t * Capability_name.t list [@@deriving sexp_of]
+type field_defn = TField of modifier * type_expr * Field_name.t * Capability_name.t list [@@deriving sexp]
 
-type capability = TCapability of mode * Capability_name.t [@@deriving sexp_of]
+type capability = TCapability of mode * Capability_name.t [@@deriving sexp]
   
 let string_of_cap (TCapability (mode, cap_name)) =
   sprintf "%s %s" (string_of_mode mode) (Capability_name.to_string cap_name)
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
 type param = (*type_expr * Var_name.t * Capability_name.t list option * borrowed_ref option*)
-  | Param of type_expr * Var_name.t [@@deriving sexp_of]
+  | Param of type_expr * Var_name.t [@@deriving sexp]
 
 let get_params_types params =
   List.map ~f:(fun (Param (param_type, _)) -> param_type) params
-  [@@deriving sexp_of]
+  [@@deriving sexp]
 
 type bin_op =
   | BinOpPlus
@@ -102,7 +106,7 @@ type bin_op =
   | BinOpOr
   | BinOpEq
   | BinOpNotEq
-[@@deriving sexp_of]
+[@@deriving sexp]
 
   let string_of_bin_op = function
   | BinOpPlus          -> "+"
@@ -119,10 +123,10 @@ type bin_op =
   | BinOpEq            -> "=="
   | BinOpNotEq         -> "!="
 
-type un_op = UnOpNot | UnOpNeg [@@deriving sexp_of]
+type un_op = UnOpNot | UnOpNeg [@@deriving sexp]
 
 let string_of_un_op = function UnOpNot -> "!" | UnOpNeg -> "-"
-[@@deriving sexp_of]
+[@@deriving sexp]
 
 
 
