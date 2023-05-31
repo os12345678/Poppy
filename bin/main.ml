0a5e850df46b6a808fe0473aa4b19e7b8fe70d30
Oliver Salman <oliversalman1@gmail.com>
2023-05-31 11:19:52 +1000

    parse command line args

--- bin/main.ml
+++ bin/main.ml
@@ -1,48 +1,76 @@
 open Core
-open Poppy_parser.Lex_and_parse
-(* open Poppy_codegen.Codegen *)
-(* open Poppy_codegen.Codegen_util *)
-(* open Poppy_type_checker *)
-(* open Llvm *)
-(* open Parser_interface *)
+(* open Lexer *)
+(* open Lexing *)
+(* open Parser *)
+open Poppy_parser
+open Poppy_type_checker
+(* open Core_unix *)
 
+let is_poppy_file filename = 
+  String.split_on_chars ~on:['.'] filename |>  List.last_exn |> String.equal "poppy"
 
-exception ParseError of string [@@ocaml.warning "-38"]
+(* let get_output_file filename = 
+  String.substr_replace_all filename ~pattern:".poppy" ~with_:".ir" *)
 
-(* Read the input file *)
-let read_file filename =
-  In_channel.read_all filename
+  (* let maybe_pprint_ast should_pprint p_print ast =
+    if should_pprint then
+      ast
+      |> p_print
+      |> Sexp.to_string_hum
+      |> print_endline;
+    Result.return ast   *)
 
-  let main () =
-    try
-      (* Get the command-line arguments *)
-      let argv = Sys.get_argv () in
-      let filename = argv.(1) in
-  
-      if not (Filename.check_suffix filename ".poppy") then
-        failwith "Invalid file type. Please provide a file with the .poppy extension.";
-  
-      (* Read the input file *)
-      let input = read_file filename in
-  
-      (* Create a lexbuf from the input *)
-      let lexbuf = Lexing.from_string input in
-  
-      (* Parse the input *)
-      let ast_result = parse_program lexbuf in
-  
-      (* Handle the result of the parsing *)
-      match ast_result with
-      | Ok ast ->
-          (* Convert the AST to a string and print it *)
-          let ast_string = print_program ast in
-          print_endline ast_string
-      | Error err ->
-          (* Print the error message *)
-          print_endline (Error.to_string_hum err)
-  
-    with
-    | Failure msg -> print_endline msg
-    | _ -> print_endline "An error occurred"
+let poppy_file =
+  let error_not_file filename =
+    eprintf "'%s' is not a Poppy file. Hint: use the .poppy extension\n%!" filename ;
+    exit 1 in
+  Command.Spec.Arg_type.create (fun filename ->
+      match is_poppy_file filename with
+      | true  -> filename
+      | false -> error_not_file filename)
 
-let () = main ()
+let compile_program ?(should_pprint_past = false) ?(should_pprint_tast = false)
+?compile_out_file lexbuf =
+  let open Result in 
+  Lex_and_parse.parse_program lexbuf 
+  >>= fun ast ->
+  (if should_pprint_past then
+      print_endline (Sexp.to_string_hum (Ast.sexp_of_program ast)));
+  Type_program.type_program ast
+  >>= fun typed_ast ->
+  (if should_pprint_tast then
+      print_endline (Sexp.to_string_hum (Typed_ast.sexp_of_program typed_ast)));
+  match compile_out_file with 
+  | Some filename -> 
+    Out_channel.with_file filename ~f:(fun file_oc ->
+      (* Placeholder for IR generation *)
+      let ir_program = "IR generation not implemented yet" in
+      Out_channel.output_string file_oc ir_program;
+      Ok ())
+  | None ->
+    (* Placeholder for IR generation *)
+    let ir_program = "IR generation not implemented yet" in
+    print_endline ir_program;
+    Ok ()
+      
+let command =
+  Command.basic ~summary:"Run Poppy programs"
+    ~readme:(fun () -> "More detailed information")
+    Command.Let_syntax.(
+      let%map_open should_pprint_past =
+        flag "-printpast" no_arg ~doc:" Pretty print the parsed AST of the program"
+      and should_pprint_tast =
+        flag "-printtast" no_arg ~doc:" Pretty print the typed AST of the program"
+      and filename = anon (maybe_with_default "-" ("filename" %: poppy_file)) in
+      fun () ->
+        In_channel.with_file filename ~f:(fun file_ic ->
+            let lexbuf = Lexing.from_channel file_ic in
+            match compile_program ~should_pprint_past ~should_pprint_tast lexbuf with
+            | Ok _ -> () (* Success, do nothing *)
+            | Error e -> (* Handle error here *)
+              eprintf "Compilation error: %s\n" (Core.Error.to_string_hum e);
+              exit 1
+        )
+    ) 
+
+let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
