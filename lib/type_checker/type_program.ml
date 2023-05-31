6547e1498fd7fa05b6d2670f21ebc052081d89d8
Oliver Salman <oliversalman1@gmail.com>
2023-05-31 11:21:50 +1000

    add type_program

--- lib/type_checker/type_program.ml
+++ lib/type_checker/type_program.ml
@@ -1,1 +1,8 @@
-(* bring everything together *)
+open Core 
+open Poppy_parser
+
+let type_program (Ast.Prog (struct_defns, interface_defns, function_defns, main_expr)) = 
+  let open Result in 
+  Type_expr.type_block_expr struct_defns interface_defns function_defns main_expr []
+  >>| fun (typed_main_expr, _) ->
+    Typed_ast.Prog ([], [], [], typed_main_expr)
