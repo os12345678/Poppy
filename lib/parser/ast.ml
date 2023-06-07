aee3e8b54bf770bd1e09696b2fd54d5b73556283
Oliver Salman <oliversalman1@gmail.com>
2023-06-07 15:47:58 +1000

    update ast for traits

--- lib/parser/ast.ml
+++ lib/parser/ast.ml
@@ -18,6 +18,7 @@ and expr_node =
 | Identifier          of identifier
 | Let                 of type_expr option * Var_name.t * expr
 | Assign              of identifier * expr  
+| Constructor         of Var_name.t * Struct_name.t * constructor_arg list
 | MethodApp           of Var_name.t * Method_name.t * expr list
 | FunctionApp         of Function_name.t * expr list 
 | FinishAsync         of loc * async_expr list * block_expr
@@ -26,60 +27,75 @@ and expr_node =
 | For                 of expr * expr * expr * block_expr
 | BinOp               of bin_op * expr * expr
 | UnOp                of un_op * expr
-| NewStruct           of Struct_name.t * (Field_name.t * expr) list
-| AssignToInterface   of Var_name.t * expr
+(* | NewStruct           of Struct_name.t * (Field_name.t * expr) list *)
+(* | AssignToInterface   of Var_name.t * expr *)
 [@@deriving sexp]
 
 and block_expr = Block of loc * expr list [@@deriving sexp]
 
 and async_expr = AsyncExpr of block_expr [@@deriving sexp]
 
-(* Method defn consists of the method name, return type (and whether it returns a borrowed
-   ref), the list of params, and the capabilities used and the body expr of the function *)
-type method_defn =
-| TMethod of
-    Method_name.t
-    * borrowed_ref option
-    * type_expr
-    * param list
-    * block_expr
-  [@@deriving sexp]
+and constructor_arg = ConstructorArg of Field_name.t * expr [@@deriving sexp]
 
-(* Struct definitions consist of the struct name, its fields, and its methods *)
+(* 
+struct struct_name {
+  field_name1: type_expr1,
+  field_name2: type_expr2,
+} 
+*)
 type struct_defn = 
   | TStruct of 
   Struct_name.t 
   * capability list
   * field_defn list
-  * method_defn list  (* Method definitions associated with the struct *)
+  [@@deriving sexp]
+
+(* 
+impl trait_name for struct_name {
+  method_name(param1: type_expr1, param2: type_expr2) -> type_expr3 {
+    body_expr
+  }
+} 
+*)
+type method_defn =
+| TMethod of
+    Trait_name.t
+    * Struct_name.t option (* impl trait_name for struct_name *)
+    * Method_name.t
+    * borrowed_ref option
+    * type_expr
+    * param list
+    * block_expr
   [@@deriving sexp]
       
-(* Interface method defn only contains the method signature of method_defn *)
-type interface_method_defn =
-| TInterfaceMethod of
-    Method_name.t
+(*
+trait trait_name {
+  method_name(param1: type_expr1, param2: type_expr2) -> type_expr3;
+}
+*)
+type trait_defn =
+| TTrait of
+    Trait_name.t
+    * Method_name.t
     * borrowed_ref option
     * type_expr
     * param list
   [@@deriving sexp]
 
-(* Interface definitions consist of the interface name and its methods *)
-type interface_defn = 
-  | TInterface of 
-  Interface_name.t 
-  * interface_method_defn list  (* Include method names in the signature *)
-  [@@deriving sexp]
-      
-(* Function defn consists of the function name, return type, the list of params, and the
-   body expr of the function *)
+(*
+fn function_name(param1: type_expr1, param2: type_expr2) -> type_expr3 {
+  body_expr
+}   
+*)
 type function_defn =
 | TFunction of
     Function_name.t * borrowed_ref option * type_expr * param list * block_expr
     [@@deriving sexp]
 
 type program = Prog of 
-                struct_defn list 
-                * interface_defn list 
-                * function_defn list 
+                (struct_defn list) 
+                * (trait_defn list) 
+                * (method_defn list) 
+                * (function_defn list) 
                 * block_expr
 [@@deriving sexp]
--- lib/type_checker/typed_ast.ml
+++ lib/type_checker/typed_ast.ml
@@ -37,52 +37,64 @@ and block_expr = Block of loc * type_expr * expr list [@@deriving sexp]
 
 and async_expr = AsyncExpr of block_expr [@@deriving sexp]
 
-(* Interface method defn only contains the method signature of method_defn *)
-type interface_method_defn =
-| TInterfaceMethod of
-    Method_name.t
-    * borrowed_ref option
-    * type_expr
-    * param list
-  [@@deriving sexp]
-
-(* Interface definitions consist of the interface name and its methods *)
-type interface_defn = 
-  | TInterface of 
-  Interface_name.t 
-  * interface_method_defn list  (* Include method names in the signature *)
-  [@@deriving sexp]
-
-(* Method defn consists of the method name, return type (and whether it returns a borrowed
-   ref), the list of params, and the capabilities used and the body expr of the function *)
-type method_defn =
-| TMethod of
-    Method_name.t
-    * borrowed_ref option
-    * type_expr
-    * param list
-    * block_expr
-  [@@deriving sexp]
-
-(* Struct definitions consist of the struct name, its fields, and its methods *)
+(* 
+struct struct_name {
+  field_name1: type_expr1,
+  field_name2: type_expr2,
+} 
+*)
 type struct_defn = 
   | TStruct of 
   Struct_name.t 
   * capability list
   * field_defn list
-  * method_defn list  (* Method definitions associated with the struct *)
+  [@@deriving sexp]
+
+(* 
+impl trait_name for struct_name {
+  fn method_name(param1: type_expr1, param2: type_expr2) -> type_expr3 {
+    body_expr
+  }
+} 
+*)
+type method_defn =
+| TMethod of
+    Trait_name.t
+    * Struct_name.t option (* impl trait_name for struct_name *)
+    * borrowed_ref option
+    * type_expr
+    * param list
+    * block_expr
   [@@deriving sexp]
       
-(* Function defn consists of the function name, return type, the list of params, and the
-   body expr of the function *)
+(*
+trait trait_name {
+  fn method_name(param1: type_expr1, param2: type_expr2) -> type_expr3;
+}
+*)
+type trait_defn =
+| TETrait of
+    Trait_name.t
+    * Method_name.t
+    * borrowed_ref option
+    * type_expr
+    * param list
+  [@@deriving sexp]
+
+(*
+fn function_name(param1: type_expr1, param2: type_expr2) -> type_expr3 {
+  body_expr
+}   
+*)
 type function_defn =
 | TFunction of
     Function_name.t * borrowed_ref option * type_expr * param list * block_expr
     [@@deriving sexp]
 
 type program = Prog of 
-                struct_defn list 
-                * interface_defn list 
-                * function_defn list 
-                * block_expr 
-                [@@deriving sexp]
+                struct_defn list
+                * trait_defn list
+                * method_defn list
+                * function_defn list
+                * block_expr
+[@@deriving sexp]
