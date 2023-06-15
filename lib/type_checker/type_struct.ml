a06b578a57e8fe14d47f9395bba6ad21d8f3a8a4
Oliver Salman <oliversalman1@gmail.com>
2023-06-15 21:56:50 +1000

    rename + add struct typing logic

--- lib/type_checker/type_struct.ml
+++ lib/type_checker/type_struct.ml
@@ -2,11 +2,11 @@ open Poppy_parser
 open Type_env
 open Core 
 
+(* Check struct invariances *)
+
 let check_no_duplicate_struct_names struct_defn =
   let struct_names = List.map ~f:(fun (Ast.TStruct (struct_name, _, _)) -> struct_name) struct_defn in
-  let struct_names_as_strings = List.map ~f:Ast_types.Struct_name.to_string struct_names in
-  if List.contains_dup ~compare:String.compare struct_names_as_strings
-  then
+  if has_duplicates struct_names ~equal:Ast_types.Struct_name.(=)  then
     Or_error.errorf "Duplicate struct names found"
   else
     Ok ()
@@ -14,9 +14,7 @@ let check_no_duplicate_struct_names struct_defn =
 let check_no_duplicate_fields struct_defn =
   let field_defn_lists = List.map ~f:(fun (Ast.TStruct (_, _, field_defn_list)) -> field_defn_list) struct_defn in
   let field_names = List.concat_map ~f:(List.map ~f:(function Ast_types.TField (_, _, field_name, _) -> field_name)) field_defn_lists in
-  let field_names_as_strings = List.map ~f:Ast_types.Field_name.to_string field_names in
-  if List.contains_dup ~compare:String.compare field_names_as_strings
-  then
+  if has_duplicates field_names ~equal:Ast_types.Field_name.(=)  then
     Or_error.errorf "Duplicate field names found"
   else
     Ok ()
@@ -25,39 +23,32 @@ let type_field_defn struct_defn =
   let check_field (Ast_types.TField (_modifier, type_expr, _field_name, _capability_list)) =
     check_type_valid struct_defn type_expr
   in
-  let check_struct (Ast.TStruct (_name, _capability_list, field_defn_list)) =
+  let check_struct (Ast.TStruct (_, _, field_defn_list)) =
     List.map ~f:check_field field_defn_list
   in
   List.concat_map ~f:check_struct struct_defn
   |> Result.all_unit
 
-(* TODO: type capabilities in struct body *)
-    
-(* 
-struct struct_name {
-  field_name1: type_expr1,
-  field_name2: type_expr2,
-} 
-*)
-(* type struct_defn = 
-  | TStruct of 
-  Struct_name.t 
-  * capability list
-  * field_defn list
-  [@@deriving sexp] *)
+let init_env_from_params params =
+  let param_pairs = List.map
+    ~f:(function Ast_types.Param (type_expr, param_name, _, _) -> (param_name, type_expr))
+    params in
+  match VarNameMap.of_alist param_pairs with
+  | `Duplicate_key _ -> Error (Base.Error.of_string "Duplicate parameter names found")
+  | `Ok map -> Ok [map]
 
-  let type_struct_defn (Ast.TStruct (struct_name, capability_list, field_defn_list) as current_struct_defn) 
-  : (Typed_ast.struct_defn, Error.t) Result.t= 
-    let open Result in
-    check_no_duplicate_struct_names [current_struct_defn] >>= fun _ ->
-    check_no_duplicate_fields [current_struct_defn] >>= fun _ ->
-    type_field_defn [current_struct_defn] >>= fun _ ->
-    let typed_struct_defn = Typed_ast.TStruct (struct_name, capability_list, field_defn_list) in
-    Ok typed_struct_defn
+let type_struct_defn (Ast.TStruct (struct_name, capability_list, field_defn_list) as current_struct_defn) = 
+  let open Result in
+  let open Core.Result.Let_syntax in
+  let%bind () = check_no_duplicate_struct_names [current_struct_defn] in
+    let%bind () = check_no_duplicate_fields [current_struct_defn] in
+      let%bind () = type_field_defn [current_struct_defn] in
+  let typed_struct_defn = Typed_ast.TStruct (struct_name, capability_list, field_defn_list) in
+  Ok typed_struct_defn
   
 let type_struct_defns struct_defns = 
-  let open Result in
-  check_no_duplicate_struct_names struct_defns >>= fun _ ->
-  check_no_duplicate_fields struct_defns >>= fun _ ->
+  let open Core.Result.Let_syntax in
+  let%bind () = check_no_duplicate_struct_names struct_defns in
+    let%bind () = check_no_duplicate_fields struct_defns in
   Result.all (List.map ~f:type_struct_defn struct_defns)
     
