5fcee667e48b047c4946a40a35aee069e196ddd9
Oliver Salman <oliversalman1@gmail.com>
2023-10-16 18:07:30 +1100

    update data race program

--- lib/data-race-checker/type_data_race_program.ml
+++ lib/data-race-checker/type_data_race_program.ml
@@ -1,15 +1,18 @@
 open Core
-open Type_data_race_expr
 open Poppy_type_checker.Type_env
+open Poppy_type_checker.Typed_ast
 
-module T = Poppy_type_checker.Typed_ast
-(* module E = Data_race_checker.Type_data_race_expr *)
+open Type_data_race_expr
+open Type_data_race_function
 
 let type_data_race_program (env: env)
-(T.Prog (struct_defns, trait_defns, method_defns, function_defns, main_expr)) : (T.program, Error.t) Result.t = 
+(Prog (struct_defns, trait_defns, method_defns, function_defns, main_expr)) : (program, Error.t) Result.t = 
   let open Result in
-  match type_data_races_block_expr env main_expr [] with
-  | Ok data_race_checked_main_expr -> 
-    Ok (T.Prog (struct_defns, trait_defns, method_defns, function_defns, data_race_checked_main_expr))
-  | Error err -> 
-    Error err
+    Result.all
+    (List.map
+      ~f:(fun function_defn -> type_data_races_function_defn env function_defn)
+      function_defns)
+    >>= fun data_race_checked_function_defns ->
+      type_data_races_block_expr env main_expr [] 
+      >>| fun data_race_checked_main_expr ->
+        (Prog (struct_defns, trait_defns, method_defns, data_race_checked_function_defns, data_race_checked_main_expr))
