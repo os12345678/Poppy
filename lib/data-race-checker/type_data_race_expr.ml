162e6344be778515bc092737eae244be02fc4237
Oliver Salman <oliversalman1@gmail.com>
2023-11-06 19:28:41 +1100

    type subord capabilities

--- lib/data-race-checker/type_data_race_expr.ml
+++ lib/data-race-checker/type_data_race_expr.ml
@@ -4,14 +4,19 @@ open Type_read_capabilities
 open Type_linear_capabilities
 open Type_borrowing
 open Type_async_capabilities
+open Type_subord_capabilities
+open Type_consume
 
-let type_data_races_block_expr env (block_expr: block_expr) _obj_vars_and_capabilities =
+let type_data_races_block_expr env (block_expr: block_expr) obj_vars_and_capabilities =
   let open Result in
-  let typed_block_expr = type_read_capabilities_block_expr block_expr in
-  type_linear_capabilities_block_expr env typed_block_expr
+  type_read_capabilities_block_expr block_expr
+  |> type_subord_capabilities_block_expr env obj_vars_and_capabilities
+  |> type_async_capabilities_block_expr env 
+  |> type_linear_capabilities_block_expr env 
   >>= fun typed_block_expr ->
-  let typed_block_expr = type_async_capabilities_block_expr env typed_block_expr in
-  type_function_forward_borrowing_block_expr env typed_block_expr
+    Result.ignore_m (type_consume_block_expr env typed_block_expr [])
+  >>= fun () ->
+    type_function_forward_borrowing_block_expr env typed_block_expr
   >>= fun () ->
   Result.ignore_m (type_assign_borrowed_block_expr env typed_block_expr)
   >>= fun () ->
--- lib/data-race-checker/type_subord_capabilities.ml
+++ lib/data-race-checker/type_subord_capabilities.ml
@@ -30,12 +30,12 @@ let remove_subord_capabilities_id env id =
         , remove_subord_capabilities env obj_struct caps
         , maybeBorrowed )
 
-(* let rec remove_subord_capabilities_expr env expr =
+let rec remove_subord_capabilities_expr env expr =
   match expr.node with
   | TInt _ | TBoolean _ -> expr
-  | TIdentifier id -> TIdentifier (remove_subord_capabilities_id env id)
-  | TBlockExpr block_expr ->
-    TBlockExpr (remove_subord_capabilities_block_expr env block_expr)
+  | TIdentifier id -> {expr with node = TIdentifier (remove_subord_capabilities_id env id)}
+  | TBlockExpr block_expr -> {expr with node = 
+    TBlockExpr (remove_subord_capabilities_block_expr env block_expr)}
   | TConstructor (var_name, class_name, constructor_args) ->
       let updated_args =
         List.map
@@ -43,4 +43,69 @@ let remove_subord_capabilities_id env id =
             ConstructorArg
               (field_name, remove_subord_capabilities_expr env expr))
           constructor_args in
-      TConstructor (var_name, class_name, updated_args) *)
+      {expr with node = TConstructor (var_name, class_name, updated_args)}
+  | TLet (type_expr, var_name, bound_expr) ->
+    {expr with node = TLet
+      (type_expr, var_name, remove_subord_capabilities_expr env bound_expr)}
+  | TAssign (id, assigned_expr) ->
+      {expr with node = TAssign
+        ( remove_subord_capabilities_id env id
+        , remove_subord_capabilities_expr env assigned_expr )}
+  | TConsume id -> {expr with node = TConsume (remove_subord_capabilities_id env id)}
+  | TMethodApp (obj_name, obj_struct, obj_trait, method_name, obj_capabilities, args)
+  -> {expr with node = 
+    TMethodApp
+      ( obj_name
+      , obj_struct
+      , obj_trait
+      , method_name
+      , remove_subord_capabilities env obj_struct obj_capabilities
+      , List.map ~f:(remove_subord_capabilities_expr env) args )}
+  | TFunctionApp (func_name, args) -> {expr with node = 
+    TFunctionApp
+      ( func_name
+      , List.map ~f:(remove_subord_capabilities_expr env) args )}
+  | TPrintf (format_str, args) -> {expr with node = 
+    TPrintf
+      (format_str, List.map ~f:(remove_subord_capabilities_expr env) args)}
+  | TFinishAsync (async_exprs, curr_thread_free_vars, curr_thread_expr) ->
+    {expr with node = TFinishAsync
+      ( List.map
+          ~f:(fun (AsyncExpr (free_vars, expr)) ->
+            AsyncExpr (free_vars, remove_subord_capabilities_block_expr env expr))
+          async_exprs
+      , curr_thread_free_vars
+      , remove_subord_capabilities_block_expr env curr_thread_expr )}
+  | TIf (cond_expr, then_expr, else_expr) ->
+    {expr with node = TIf
+      ( remove_subord_capabilities_expr env cond_expr
+      , remove_subord_capabilities_block_expr env then_expr
+      , remove_subord_capabilities_block_expr env else_expr )}
+  | TWhile (cond_expr, loop_expr) ->
+    {expr with node = TWhile
+      ( remove_subord_capabilities_expr env cond_expr
+      , remove_subord_capabilities_block_expr env loop_expr )}
+  | TBinOp (binop, expr1, expr2) ->
+    {expr with node = TBinOp
+      ( binop
+      , remove_subord_capabilities_expr env expr1
+      , remove_subord_capabilities_expr env expr2 )}
+  | TUnOp (unop, expr) ->
+      {expr with node = TUnOp (unop, remove_subord_capabilities_expr env expr)}
+
+and remove_subord_capabilities_block_expr env
+    (Block (loc, type_block_expr, exprs)) =
+  let updated_exprs = List.map ~f:(remove_subord_capabilities_expr env) exprs in
+  Block (loc, type_block_expr, updated_exprs)
+
+let is_this_present obj_vars_and_capabilities =
+  let obj_var_names, _, _ = List.unzip3 obj_vars_and_capabilities in
+  elem_in_list (Var_name.of_string "this") obj_var_names
+  
+
+(* If we are not in an object method then we can't access subordinate state, so remove
+   access to subordinate state. We do this by checking if "this" is a param of the method. *)
+let type_subord_capabilities_block_expr class_defns obj_vars_and_capabilities block_expr =
+  if is_this_present obj_vars_and_capabilities then block_expr
+  else remove_subord_capabilities_block_expr class_defns block_expr
+    
